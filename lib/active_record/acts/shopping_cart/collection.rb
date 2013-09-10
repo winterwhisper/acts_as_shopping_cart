9c1fd58f593aa86bde36093af13ecca496d505a8
winterwhisper <winterwhisper.dev@gmail.com>
2013-09-10 16:58:45 +0800

    add opts arg to cart item create method

--- lib/active_record/acts/shopping_cart/collection.rb
+++ lib/active_record/acts/shopping_cart/collection.rb
@@ -5,11 +5,11 @@ module ActiveRecord
         #
         # Adds a product to the cart
         #
-        def add(object, price, quantity = 1, cumulative = true)
+        def add(object, price, quantity = 1, opts = {}, cumulative = true)
           cart_item = item_for(object)
 
           unless cart_item
-            shopping_cart_items.create(:item => object, :price => price, :quantity => quantity)
+            shopping_cart_items.create({:item => object, :price => price, :quantity => quantity}.merge(opts))
           else
             cumulative = cumulative == true ? cart_item.quantity : 0
             cart_item.quantity = (cumulative + quantity)
